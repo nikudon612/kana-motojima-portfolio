e3fe814233937a2c1af76411da290a3662e5ca3e
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2024-07-15 13:34:16 -0400

    added sanity env info and fetch code as well as basic photo fetch for homepage gallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,34 @@
-<script></script>
+<script>
+  export let data;
+  export let error;
+  console.log("homepageGallery in script:", data); // Debugging log
+</script>
 
 <svelte:head>
-    <title>Kana Motojima</title>
+  <title>Kana Motojima</title>
 </svelte:head>
 
-<div>
-    <h1>Homepage</h1>
-</div>
+{#if error}
+  <p>{error}</p>
+{:else if data}
+  <div>
+    <h1>{data.props.data.title}</h1>
+    <p>{data.props.data.description}</p>
+    <div class="relative w-full h-screen">
+      {#each data.props.data.photos as { title, imageUrl }}
+        <div class="photo">
+          <h2>{title}</h2>
+          <img src={imageUrl} alt={title} />
+        </div>
+      {/each}
+    </div>
+  </div>
+{:else}
+  <p>Loading...</p>
+{/if}
+
+<style>
+  .photo {
+    position: absolute;
+  }
+</style>
