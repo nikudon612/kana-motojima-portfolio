b5b7a9ef669aae95331a2d7303561e4b9afab862
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2024-07-15 16:42:39 -0400

    navbar basic layout setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,20 @@
-<nav class="navbar">
-  <div>
-    <a href="/work">work</a>
-    <a href="/contact">contact</a>
-  </div>
-  <div>
-    <a href="/">Kana Motojima</a>
-  </div>
-</nav>
-
-<slot />
-
-<style>
-  .navbar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 1rem;
-    color: black;
-    background-color: transparent; /* Make it invisible */
-    font-family: Arial, sans-serif;
-  }
-  .navbar a {
-    color: inherit;
-    text-decoration: none;
-  }
-</style>
+<script>
+    import '../app.css';
+  </script>
+  
+  <nav class="fixed bottom-0 left-0 w-full flex justify-between p-4 text-black bg-transparent">
+    <div class="flex flex-col ml-8 text-[1.125rem]">
+      <a href="/work" class="mb-2">work</a>
+      <a href="/contact">contact</a>
+    </div>
+    <div class="mr-8 flex flex-col items-start justify-end">
+      <a href="/" class="text-[1.5rem]">Kana Motojima</a>
+    </div>
+  </nav>
+  
+  <slot />
+  
+  <style>
+    /* Any additional styles can go here */
+  </style>
+  
