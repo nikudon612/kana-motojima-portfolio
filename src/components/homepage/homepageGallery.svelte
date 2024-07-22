f167dfd84c43283c26b320864f220544a4a67cfe
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2024-07-22 18:28:15 -0400

    update to kana text color dynamically

--- src/components/homepage/homepageGallery.svelte
+++ src/components/homepage/homepageGallery.svelte
@@ -16,25 +16,13 @@
   }
 </script>
 
-<div
-  class="w-full h-screen relative mobile:flex mobile:flex-col mobile:items-center mobile:gap-[2rem] mobile:px-[2rem]"
->
+<div class="w-full h-screen relative mobile:flex mobile:flex-col mobile:items-center">
   {#each images as { title, imageUrl, x, y }, index}
-    <div
-      class="absolute mobile:static cursor-pointer"
-      style="left: {x}px; top: {y}px;"
-      on:click={() => handleImageClick(index)}
-    >
-      <img
-        src={imageUrl}
-        alt={title}
-        class="desktop:max-w-[275px] w-full h-auto object-contain"
-      />
+    <div class="absolute mobile:static cursor-pointer w-full" style="left: {x}px; top: {y}px;" on:click={() => handleImageClick(index)}>
+      <img src={imageUrl} alt={title} class="w-full h-auto object-cover" />
     </div>
   {/each}
-  <div
-    class="hidden mobile:block fixed right-[-3.5rem] bottom-[6rem] text-lg font-bold z-2001 -rotate-90"
-  >
+  <div class="hidden mobile:block fixed right-[-3rem] bottom-[6rem] text-lg font-bold z-2001 -rotate-90 kana-text">
     <a href="/">Kana Motojima</a>
   </div>
 </div>
@@ -42,3 +30,22 @@
 {#if showModal}
   <SlideshowModal {images} {currentIndex} on:close={closeModal} />
 {/if}
+
+<style>
+  .mobile\:gap-0 {
+    gap: 0 !important;
+  }
+  .mobile\:px-0 {
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+  }
+
+  .kana-text a {
+    color: rgb(47, 47, 47); /* Set initial color to white */
+    mix-blend-mode: exclusion; /* This will invert the color based on the background */
+    background: transparent; /* Ensure background is transparent */
+    padding: 0.5rem; /* Optional: add padding for better visibility */
+    border-radius: 5px; /* Optional: add border radius for aesthetics */
+    white-space: nowrap; /* Ensure the text doesn't wrap */
+  }
+</style>
