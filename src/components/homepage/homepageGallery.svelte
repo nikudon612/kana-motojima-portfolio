ffc9f5026824c5c516a805751e0ff300e5454782
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2024-07-22 18:10:11 -0400

    added mobile navbar and name

--- src/components/homepage/homepageGallery.svelte
+++ src/components/homepage/homepageGallery.svelte
@@ -1,5 +1,5 @@
 <script>
-  import SlideshowModal from './slideshowModal.svelte';
+  import SlideshowModal from "./slideshowModal.svelte";
   export let data;
   let showModal = false;
   let currentIndex = 0;
@@ -11,17 +11,32 @@
   }
 
   function closeModal() {
-    console.log('Modal closed');
+    console.log("Modal closed");
     showModal = false;
   }
 </script>
 
-<div class="w-full h-screen relative mobile:flex mobile:flex-col mobile:items-center mobile:gap-2.5">
+<div
+  class="w-full h-screen relative mobile:flex mobile:flex-col mobile:items-center mobile:gap-[2rem] mobile:px-[2rem]"
+>
   {#each images as { title, imageUrl, x, y }, index}
-    <div class="absolute mobile:static cursor-pointer" style="left: {x}px; top: {y}px;" on:click={() => handleImageClick(index)}>
-      <img src={imageUrl} alt={title} class="desktop:max-w-[275px] w-full h-auto object-contain" />
+    <div
+      class="absolute mobile:static cursor-pointer"
+      style="left: {x}px; top: {y}px;"
+      on:click={() => handleImageClick(index)}
+    >
+      <img
+        src={imageUrl}
+        alt={title}
+        class="desktop:max-w-[275px] w-full h-auto object-contain"
+      />
     </div>
   {/each}
+  <div
+    class="hidden mobile:block fixed right-[-3.5rem] bottom-[6rem] text-lg font-bold z-2001 -rotate-90"
+  >
+    <a href="/">Kana Motojima</a>
+  </div>
 </div>
 
 {#if showModal}
