d92459fe6c83cfc082868db69bddca08d4c38481
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2024-08-05 14:38:05 -0400

    added basic 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Page Not Found</title>
+</svelte:head>
+
+<div class="h-full w-full px-[2rem]">
+  <h1 class="font-bold">404 Page Not Found</h1>
+  <p>Sorry, the page you are looking for does not exist.</p>
+  <a href="/">Go back to the homepage</a>
+</div>
