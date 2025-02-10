4f07030bd9c898d736a5ecb2bc50786d4d2e6111
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-02-10 00:23:41 -0500

    updated site font and weights, in use on navigation and across entire site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  safelist: [
+    "font-thonglor-semi-bold", // Safelist your custom class
   ],
   theme: {
     extend: {
+      fontFamily: {
+        thonglor: ['"thonglor-soi-4"', "sans-serif"], // Add custom font
+        "thonglor-semi-bold": ['"thonglor-soi-4"', "sans-serif"], // Semi-bold alias
+      },
+      fontWeight: {
+        "semi-bold": "600", // Adjust based on available font weights
+        bold: "700",
+      },
       screens: {
-        'mobile': {'max': '768px'},
-        'tablet': {'min': '769px', 'max': '1024px'},
-        'desktop': {'min': '1025px'},
+        mobile: { max: "768px" },
+        tablet: { min: "769px", max: "1024px" },
+        desktop: { min: "1025px" },
       },
     },
   },
